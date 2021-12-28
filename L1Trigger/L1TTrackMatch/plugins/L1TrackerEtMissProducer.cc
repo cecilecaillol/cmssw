5da059d2fbf576404a550308481385d4dc461a7d
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2021-12-28 11:01:44 -0600

    Changes to L1TrackerEtMissProducer if the L1TrackSelectionProducer is run first.
    (cherry picked from commit e1dda8deb549973d6e32de0367142625ddbc091e)

--- L1Trigger/L1TTrackMatch/plugins/L1TrackerEtMissProducer.cc
+++ L1Trigger/L1TTrackMatch/plugins/L1TrackerEtMissProducer.cc
@@ -28,7 +28,11 @@ class L1TrackerEtMissProducer : public edm::global::EDProducer<> {
 public:
   typedef TTTrack<Ref_Phase2TrackerDigi_> L1TTTrackType;
   typedef std::vector<L1TTTrackType> L1TTTrackCollectionType;
-  typedef edm::RefVector<L1TTTrackCollectionType> L1TTTrackRefCollectionType;
+  typedef edm::Ref<L1TTTrackCollectionType> L1TTTrackRefType;
+  typedef std::vector<L1TTTrackRefType> L1TTTrackRefCollectionType;
+
+  typedef Vertex L1VertexType;
+  typedef VertexCollection L1VertexCollectionType;
 
   explicit L1TrackerEtMissProducer(const edm::ParameterSet&);
   ~L1TrackerEtMissProducer() override = default;
@@ -37,30 +41,54 @@ private:
   void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
 
   // ----------member data ---------------------------
+  float deltaZ_;  // in cm
+  float maxPt_;       // in GeV
+  int highPtTracks_;  // saturate or truncate
+  bool displaced_;    // prompt/displaced tracks
+
+  vector<double> z0Thresholds_;  // Threshold for track to vertex association
+  vector<double> etaRegions_;    // Eta bins for choosing deltaZ threshold
+  bool debug_;
+
+  std::string L1MetCollectionName;
+  std::string L1ExtendedMetCollectionName;
+
+  const edm::EDGetTokenT<VertexCollection> pvToken_;
   const edm::EDGetTokenT<L1TTTrackRefCollectionType> trackToken_;
-  const edm::EDGetTokenT<L1TTTrackRefCollectionType> vtxAssocTrackToken_;
-  const std::string L1MetCollectionName;
-  const float maxPt_;       // in GeV
-  const int highPtTracks_;  // saturate or truncate
-  const bool debug_;
 };
 
 // constructor
 L1TrackerEtMissProducer::L1TrackerEtMissProducer(const edm::ParameterSet& iConfig)
-    : trackToken_(consumes<L1TTTrackRefCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackInputTag"))),
-      vtxAssocTrackToken_(
-          consumes<L1TTTrackRefCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackAssociatedInputTag"))),
-      L1MetCollectionName(iConfig.getParameter<std::string>("L1MetCollectionName")),
-      maxPt_(iConfig.getParameter<double>("maxPt")),
-      highPtTracks_(iConfig.getParameter<int>("highPtTracks")),
-      debug_(iConfig.getParameter<bool>("debug")) {
-  produces<TkEtMissCollection>(L1MetCollectionName);
+    : pvToken_(consumes<L1VertexCollectionType>(iConfig.getParameter<edm::InputTag>("L1VertexInputTag"))),
+      trackToken_(consumes<L1TTTrackRefCollectionType>(iConfig.getParameter<edm::InputTag>("L1TrackInputTag"))) {
+  deltaZ_ = (float)iConfig.getParameter<double>("deltaZ");
+  maxPt_ = (float)iConfig.getParameter<double>("maxPt");
+  highPtTracks_ = iConfig.getParameter<int>("highPtTracks");
+  displaced_ = iConfig.getParameter<bool>("displaced");
+  z0Thresholds_ = iConfig.getParameter<std::vector<double>>("z0Thresholds");
+  etaRegions_ = iConfig.getParameter<std::vector<double>>("etaRegions");
+
+  debug_ = iConfig.getParameter<bool>("debug");
+
+  L1MetCollectionName = (std::string)iConfig.getParameter<std::string>("L1MetCollectionName");
+
+  if (displaced_) {
+    L1ExtendedMetCollectionName = (std::string)iConfig.getParameter<std::string>("L1MetExtendedCollectionName");
+    produces<TkEtMissCollection>(L1ExtendedMetCollectionName);
+  } else
+    produces<TkEtMissCollection>(L1MetCollectionName);
 }
 
 void L1TrackerEtMissProducer::produce(edm::StreamID, edm::Event& iEvent, const edm::EventSetup& iSetup) const {
   using namespace edm;
 
   std::unique_ptr<TkEtMissCollection> METCollection(new TkEtMissCollection);
+
+  // Tracker Topology
+  const TrackerTopology& tTopo = iSetup.getData(tTopoToken_);
+
+  edm::Handle<L1VertexCollectionType> L1VertexHandle;
+  iEvent.getByToken(pvToken_, L1VertexHandle);
 
   edm::Handle<L1TTTrackRefCollectionType> L1TTTrackHandle;
   iEvent.getByToken(trackToken_, L1TTTrackHandle);
@@ -88,9 +116,11 @@ void L1TrackerEtMissProducer::produce(edm::StreamID, edm::Event& iEvent, const e
   int numqualitytracks = 0;
   int numassoctracks = 0;
 
-  for (const auto& track : *L1TTTrackHandle) {
+  for (const auto & track : *L1TTTrackHandle) {
     float pt = track->momentum().perp();
     float phi = track->momentum().phi();
+    float eta = track->momentum().eta();
+    float z0 = track->z0();
 
     if (maxPt_ > 0 && pt > maxPt_) {
       if (highPtTracks_ == 0)
@@ -101,8 +131,21 @@ void L1TrackerEtMissProducer::produce(edm::StreamID, edm::Event& iEvent, const e
 
     numqualitytracks++;
 
-    if (std::find(L1TTTrackAssociatedHandle->begin(), L1TTTrackAssociatedHandle->end(), track) !=
-        L1TTTrackAssociatedHandle->end()) {
+    if (!displaced_) {  // if displaced, deltaZ = 3.0 cm, very loose
+      // construct deltaZ cut to be based on track eta
+      for (unsigned int reg = 0; reg < etaRegions_.size(); reg++) {
+        if (std::abs(eta) >= etaRegions_[reg] && std::abs(eta) < etaRegions_[reg + 1]) {
+          deltaZ_ = z0Thresholds_[reg];
+          break;
+        }
+      }
+      if (std::abs(eta) >= etaRegions_[etaRegions_.size() - 1]) {
+        deltaZ_ = z0Thresholds_[etaRegions_.size() - 1];
+        break;
+      }
+    }
+
+    if (std::abs(z0 - zVTX) <= deltaZ_) {
       numassoctracks++;
       sumPx += pt * cos(phi);
       sumPy += pt * sin(phi);
@@ -127,9 +170,9 @@ void L1TrackerEtMissProducer::produce(edm::StreamID, edm::Event& iEvent, const e
                                                 << "\n"
                                                 << "MET: " << et << "| Phi: " << etphi << "\n"
 
-                                                << "# Tracks after quality cuts: " << L1TTTrackHandle->size() << "\n"
+                                                << "# Tracks after Quality Cuts: " << L1TTTrackHandle->size() << "\n"
                                                 << "# Tacks after additional highPt Cuts: " << numqualitytracks << "\n"
-                                                << "# Tracks associated to vertex: " << numassoctracks << "\n"
+                                                << "# Tracks Associated to Vertex: " << numassoctracks << "\n"
                                                 << "========================================================"
                                                 << "\n";
   }
