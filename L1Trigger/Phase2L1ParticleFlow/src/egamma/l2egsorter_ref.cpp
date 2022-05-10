0b4d46e0a50c6c6c16addbaa2b37f394b3944a1b
ccaillol <ccaillol@cern.ch>
2022-05-10 21:32:36 +0200

    add forgotten file

--- L1Trigger/Phase2L1ParticleFlow/src/egamma/l2egsorter_ref.cpp
+++ L1Trigger/Phase2L1ParticleFlow/src/egamma/l2egsorter_ref.cpp
@@ -14,8 +14,7 @@ using namespace l1ct;
 l1ct::L2EgSorterEmulator::L2EgSorterEmulator(const edm::ParameterSet &pset)
     : L2EgSorterEmulator(pset.getParameter<uint32_t>("nBOARDS"),
                          pset.getParameter<uint32_t>("nEGPerBoard"),
-                         pset.getParameter<uint32_t>("nEGOut"),
-                         pset.getUntrackedParameter<uint32_t>("debug", 0)) {}
+                         pset.getParameter<uint32_t>("nEGOut")) {}
 
 #endif
 
@@ -43,15 +42,6 @@ void L2EgSorterEmulator::toFirmware(const std::vector<EGIsoObjEmu> &out_photons,
 void L2EgSorterEmulator::run(const std::vector<l1ct::OutputBoard> &in,
                              std::vector<EGIsoObjEmu> &out_photons,
                              std::vector<EGIsoEleObjEmu> &out_eles) const {
-  if (debug_) {
-    unsigned int board_n = 0;
-    for (const auto &board : in) {
-      dbgCout() << "BOARD " << board_n++ << std::endl;
-      print_objects(board.egphoton, "photon_in");
-      print_objects(board.egelectron, "electron_in");
-    }
-  }
-
   // we copy to be able to resize them
   std::vector<std::vector<EGIsoObjEmu>> photons_in;
   std::vector<std::vector<EGIsoEleObjEmu>> eles_in;
@@ -68,9 +58,4 @@ void L2EgSorterEmulator::run(const std::vector<l1ct::OutputBoard> &in,
   }
   merge(photons_in, out_photons);
   merge(eles_in, out_eles);
-
-  if (debug_) {
-    print_objects(out_photons, "photon_out");
-    print_objects(out_eles, "electron_out");
-  }
 }
